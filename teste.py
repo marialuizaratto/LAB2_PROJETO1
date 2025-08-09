a209bdc2f9943db12801590130b0fd066a0f67c8
Maria Luiza Ratto <marialuizamratto@gmail.com>
2025-08-09 17:26:48 -0300

    Adiciona função sorteia_media que sorteia 7 números e calcula média

--- teste.py
+++ teste.py
@@ -2,3 +2,11 @@ def cumprimento(texto):
     return f"Olá, {texto}"
 print(cumprimento("Maria Luiza Ratto"))
 
+import random
+
+def sorteia_media():
+    numeros = [random.randint(1, 100) for _ in range(7)]  
+    media = sum(numeros) / len(numeros)
+    return media
+
+print("Média dos 7 números sorteados:", sorteia_media())
